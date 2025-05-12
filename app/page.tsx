4d19ad290a8e84b02bbda127c7b83d5c5f00242e
junaidwebs <junaidwebs@gmail.com>
2025-05-12 21:12:17 +0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,22 @@
+"use client"
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import pkr from '@/app/ui/home.module.css';
+import { useState } from 'react';
+import { lusitana } from './ui/fonts';
+import clsx from 'clsx';
 export default function Page() {
+  const [state, setState] = useState(2);
+  let status = 1;
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
+            <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <p className={` ${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
@@ -28,6 +34,12 @@ export default function Page() {
           {/* Add Hero Images Here */}
         </div>
       </div>
+      <div   className={clsx(
+        {
+        "red" : state == 1,
+        "green" : state == 2,
+      })}/>
     </main>
+    
   );
 }
